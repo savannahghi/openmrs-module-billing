f4df0cdecdb3716802b8a8a3e9d917d4a5fe9a3f
franqq <githae.francis@gmail.com>
2022-06-21 23:01:26 +0300

    Add ehr cashier service tests

---
+++ api/src/test/java/org/openmrs/module/ehrcashier/api/EhrcashierServiceTest.java
@@ -0,0 +1,49 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.ehrcashier.api;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.openmrs.User;
+import org.openmrs.api.UserService;
+import org.openmrs.module.ehrcashier.Item;
+import org.openmrs.module.ehrcashier.api.dao.EhrcashierDao;
+import org.openmrs.module.ehrcashier.api.impl.EhrcashierServiceImpl;
+import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+/**
+ * This is a unit test, which verifies logic in EhrcashierService. It doesn't extend
+ * BaseModuleContextSensitiveTest, thus it is run without the in-memory DB and Spring context.
+ */
+public class EhrcashierServiceTest {
+	
+	@InjectMocks
+	EhrcashierServiceImpl basicModuleService;
+	
+	@Mock
+	EhrcashierDao dao;
+	
+	@Mock
+	UserService userService;
+	
+	@Before
+	public void setupMocks() {
+		MockitoAnnotations.initMocks(this);
+	}
+}
